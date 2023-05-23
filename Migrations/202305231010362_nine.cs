2931b17e51cc6fa6a6e2df1df756b567966237d1
Wwiktoria <wiktoriasordyl@gmail.com>
2023-05-23 21:17:55 +0200

    some files

---
+++ Migrations/202305231010362_nine.cs
@@ -0,0 +1,22 @@
+﻿namespace CarRentalApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class nine : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Rentals", "Rental_RentalId", c => c.Int());
+            CreateIndex("dbo.Rentals", "Rental_RentalId");
+            AddForeignKey("dbo.Rentals", "Rental_RentalId", "dbo.Rentals", "RentalId");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Rentals", "Rental_RentalId", "dbo.Rentals");
+            DropIndex("dbo.Rentals", new[] { "Rental_RentalId" });
+            DropColumn("dbo.Rentals", "Rental_RentalId");
+        }
+    }
+}
